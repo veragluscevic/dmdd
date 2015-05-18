2075242a49e7a73d2ca82361ce54d744e978143e
veragluscevic <vera.gluscevic@gmail.com>
2015-05-18 10:04:18 -0400

    Update globals.py

--- dmdd/globals.py
+++ dmdd/globals.py
@@ -1,7 +1,4 @@
-try:
-    import numpy as np
-except ImportError:
-    np = None
+import numpy as np
     
 FNFP_PARAM_NAMES = ['fnfp_si', 'fnfp_sd', 'fnfp_anapole', 'fnfp_magdip', 'fnfp_elecdip',
                     'fnfp_LS', 'fnfp_f1', 'fnfp_f2', 'fnfp_f3', 'fnfp_si_massless', 'fnfp_sd_massless',
